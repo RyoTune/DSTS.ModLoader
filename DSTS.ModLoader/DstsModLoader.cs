5fffd172717155a8afc4de5de747612949056d57
RyoTune <zutomayofan50@gmail.com>
2025-09-13 03:42:30 -0500

    Copy data from file directly to unmanaged buffer.

--- DSTS.ModLoader/DstsModLoader.cs
+++ DSTS.ModLoader/DstsModLoader.cs
@@ -1,15 +1,25 @@
 using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
 // ReSharper disable InconsistentNaming
 
 namespace DSTS.ModLoader;
 
 public unsafe class DstsModLoader
 {
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool ReadFile(
+        SafeFileHandle hFile,
+        IntPtr lpBuffer,
+        int nNumberOfBytesToRead,
+        out int lpNumberOfBytesRead,
+        IntPtr lpOverlapped);
+    
     private delegate nint LoadFile(PackInfo* sourcePack, nint filePath, nint buffer, nint size, nint param_5, nint param_6, int param_7);
-    private readonly SHFunction<LoadFile>? _LoadFile;
+    private readonly SHFunction<LoadFile> _LoadFile;
     
     private delegate nint GetFileSize(nint param_1, nint filePath, long* size, nint param_4, int param_5);
-    private readonly SHFunction<GetFileSize>? _GetFileSize;
+    private readonly SHFunction<GetFileSize> _GetFileSize;
     
     private readonly DstsModRegistry _registry;
     
@@ -35,12 +45,17 @@ public unsafe class DstsModLoader
         if (_registry.TryGetModFile(filePathStr, out var modFile))
         {
             Log.Debug($"{nameof(LoadFile)} || Replacing: {filePath}\nMod File: {modFile}");
-            var temp = File.ReadAllBytes(modFile.Path);
-            Marshal.Copy(temp, 0, buffer, temp.Length);
-            return 1;
+            
+            using var fs = new FileStream(modFile.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            if (ReadFile(fs.SafeFileHandle, buffer, (int)modFile.Size, out _, nint.Zero))
+            {
+                return 1;
+            }
+            
+            Log.Error($"ReadFile failed.\nFile: {modFile.Path}");
         }
         
-        return _LoadFile!.Hook!.OriginalFunction(sourcePack, filePath, buffer, size, param_5, param_6, param_7);
+        return _LoadFile.Hook!.OriginalFunction(sourcePack, filePath, buffer, size, param_5, param_6, param_7);
     }
 
     private nint GetFileSizeImpl(nint param_1, nint filePath, long* size, nint param_4, int param_5)
@@ -52,11 +67,11 @@ public unsafe class DstsModLoader
             return 1;
         }
         
-        return _GetFileSize!.Hook!.OriginalFunction(param_1, filePath, size, param_4, param_5);;
+        return _GetFileSize.Hook!.OriginalFunction(param_1, filePath, size, param_4, param_5);;
     }
     
     [StructLayout(LayoutKind.Sequential)]
-    struct PackInfo
+    private struct PackInfo
     {
         public nint Name;
     }
